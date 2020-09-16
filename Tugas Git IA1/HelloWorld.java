748ac2de7d49fc293bc88f8a059bff46be2d930e
ChoiWidiaHyun <adeyusuf0987@gmail.com>
2020-09-16 12:45:06 +0700

    HelloWorld.java

---
+++ Tugas Git IA1/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Nama : Ade Yusuf Hidayat");
+		System.out.println("Nim : 6706200018");
+		System.out.println("Kelas : D3IF-44-04");
+	}
+}
